e2e93494ab53d76d514f466c2541ba4397112446
hondajohn88 <hondajohn88@gmail.com>
2019-01-10 22:20:53 -0800

    Remove OP apps

--- common.mk
+++ common.mk
@@ -136,14 +136,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OnePlusCamera \
     OnePlusGallery \
-    OnePlusCameraService \
-    SetupWizard \
-    OnePlusWizard
+    OnePlusCameraService
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/addon.d/51-oneplus-camera.sh:system/addon.d/51-oneplus-camera.sh  \
-    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
-    $(LOCAL_PATH)/prebuilts/priv-app/SetupWizard/lib/arm64/libbarhopper.so:system/priv-app/SetupWizard/lib/arm64/libbarhopper.so
+    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml
 
 # QTI
 PRODUCT_COPY_FILES += \
